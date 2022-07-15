869145cc4b76f2d5a6485bb3511c20518179a911
Oliver Muthomi <oliver.mwitia@gmail.com>
2022-07-15 11:44:35 +0300

    chore: Homescreen iOS

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,62 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
+    let greet = Greeting().greeting()
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            
+            heading()
+            
+            Spacer()
+            
+            actions()
+            
+        }
+    }
+    
+    func heading( ) -> some View {
+        Text("Network Calls and Logging in KMM")
+    }
+    
+    
+    func actions(  ) -> some View {
+        VStack{
+            
+            Button("REST Requests" ) {
+                print("Button pressed")
+            }.padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.yellow.cornerRadius(8))
+            
+            
+            Button("Socket Requests" ) {
+                print("Button pressed")
+            }.padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.yellow.cornerRadius(8))
 
-	var body: some View {
-		Text(greet)
-	}
+            
+            Button("Server Sent Events" ) {
+                print("Button pressed")
+            }.padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.yellow.cornerRadius(8))
+
+            
+            Button("GraphQl Requests" ) {
+                print("Button pressed")
+            }.padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.yellow.cornerRadius(8))
+
+        }.padding()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
